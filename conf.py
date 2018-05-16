bda05e2d0061351aafef41bdf6b1b63b40d320d6
ycbaek <ycbaek@gmail.com>
2018-05-16 11:21:56 -0400

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,8 @@
+# -- General configuration ----------------------------
+project = 'Lake Afton Public Observatory Presenter Notes'
+copyright = '&copy; 2018 Lake Afton Public Observatory'
+
+version = '2018.01.07.0'
+
+# -- Options for HTML output --------------------------
+html_theme = 'cyborg'
